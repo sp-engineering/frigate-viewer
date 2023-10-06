3294e0d3387211722bb49276b878ca7b2656a346
Szymon Piwowarczyk <szymon@piwowarczyk.net>
2023-10-06 17:54:56 +0200

    Player controls

--- views/camera-event-clip/CameraEventClip.tsx
+++ views/camera-event-clip/CameraEventClip.tsx
@@ -1,7 +1,8 @@
-import React, {useMemo} from 'react';
-import {StyleSheet} from 'react-native';
+import {IconOutline} from '@ant-design/icons-react-native';
+import React, {FC, useCallback, useMemo, useState} from 'react';
+import {StyleSheet, Text, TouchableNativeFeedback, View} from 'react-native';
 import {NavigationFunctionComponent} from 'react-native-navigation';
-import Video from 'react-native-video';
+import {ProgressInfo, VLCPlayer} from 'react-native-vlc-media-player';
 import {apiUrl} from '../../config';
 
 interface ICameraEventClipProps {
@@ -13,7 +14,100 @@ const styles = StyleSheet.create({
     width: '100%',
     height: '100%',
   },
+  playerBar: {
+    position: 'absolute',
+    left: 0,
+    bottom: 0,
+    width: '100%',
+    padding: 1,
+    backgroundColor: 'black',
+    opacity: 0.7,
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  playerBarText: {
+    fontSize: 10,
+    fontWeight: '600',
+    color: 'white',
+  },
+  playerProgressBar: {
+    height: 3,
+    flex: 1,
+    marginVertical: 1,
+    marginHorizontal: 8,
+    borderColor: 'white',
+    borderBottomWidth: 1,
+  },
+  playerProgressBarTrack: {
+    backgroundColor: 'white',
+    height: '100%',
+  },
 });
+
+interface IVideoPlayerProps {
+  clipUrl: string;
+}
+
+const formatVideoTime = (t: number) => {
+  const time = Math.round(t / 1000);
+  const minutes = Math.floor(time / 60);
+  const seconds = time % 60;
+  return `${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
+};
+
+const VideoPlayer: FC<IVideoPlayerProps> = ({clipUrl}) => {
+  const [paused, setPaused] = useState<boolean>(false);
+  const [progressInfo, setProgressInfo] = useState<ProgressInfo>();
+
+  const togglePlay = useCallback(() => {
+    setPaused(!paused);
+  }, [paused]);
+
+  const onProgress = useCallback((info: ProgressInfo) => {
+    setProgressInfo(info);
+  }, []);
+
+  const currentTimeStr = useMemo(
+    () => formatVideoTime(progressInfo?.currentTime || 0),
+    [progressInfo?.currentTime],
+  );
+
+  const durationStr = useMemo(
+    () => formatVideoTime(progressInfo?.duration || 0),
+    [progressInfo?.duration],
+  );
+
+  const percentage = useMemo(
+    () => `${(progressInfo?.position || 0) * 100}%`,
+    [progressInfo?.position],
+  );
+
+  return (
+    <View>
+      <TouchableNativeFeedback onPress={togglePlay}>
+        <VLCPlayer
+          paused={paused}
+          source={{uri: clipUrl}}
+          style={[styles.player]}
+          resizeMode="contain"
+          onProgress={onProgress}
+        />
+      </TouchableNativeFeedback>
+      <View style={[styles.playerBar]}>
+        {paused ? (
+          <IconOutline name="pause" color="white" />
+        ) : (
+          <IconOutline name="caret-right" color="white" />
+        )}
+        <Text style={[styles.playerBarText]}>{currentTimeStr}</Text>
+        <View style={[styles.playerProgressBar]}>
+          <View style={[styles.playerProgressBarTrack, {width: percentage}]} />
+        </View>
+        <Text style={[styles.playerBarText]}>{durationStr}</Text>
+      </View>
+    </View>
+  );
+};
 
 export const CameraEventClip: NavigationFunctionComponent<
   ICameraEventClipProps
@@ -23,14 +117,7 @@ export const CameraEventClip: NavigationFunctionComponent<
     [eventId],
   );
 
-  return (
-    <Video
-      source={{uri: clipUrl}}
-      controls={true}
-      style={[styles.player]}
-      resizeMode="contain"
-    />
-  );
+  return <VideoPlayer clipUrl={clipUrl} />;
 };
 
 CameraEventClip.options = () => ({
